6a16d4f64b69181fcb10d825f2a3dfec3eac089d
k-nickles <obsequiouseyes@gmail.com>
2015-09-28 21:57:36 -0700

    queue implementation
    Made queue and node objects

---
+++ queue-practice/queue.js
@@ -0,0 +1,38 @@
+
+
+
+/* Google Interview Practice #1 - Implementing Queue*/
+
+function Node(value){
+	this.val = value;
+	this.next = null; 
+}
+
+
+function Queue(){
+	this.first = null;
+	this.size = 0; 
+
+	this.enqueue = function(value){
+		var node = new Node(value, this.next);
+
+		if(this.first == null){
+			this.first = node; 
+		}else{
+
+			n = this.first;
+			while(n.next){
+				n = n.next; 
+			}
+			n.next = node; 
+		}
+		this.size++;  
+	}
+
+	this.dequeue = function(){
+		var tmp = this.first; 
+		this.first = this.first.next; 
+		this.size--; 
+		return popVal;
+	}
+}
